55d59058cd411cf9b709382f512c25086a587198
Williams392 <williamsvalle392@gmail.com>
2023-03-05 21:54:40 -0500

    subiendo_6

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+/*==================== SCROLL DESPLAZAR SECCIONES ENLACE ACTIVO ====================*/
 
 const sections = document.querySelectorAll("section[id]");
 
@@ -53,7 +53,7 @@ window.addEventListener("scroll", scrollActive);
 /*==================== SHOW SCROLL TOP ====================*/
 function scrollTop() {
   const scrollTop = document.getElementById("scroll-top");
-  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  // Cuando el desplazamiento es superior a la altura de la ventana gráfica 560, agregue la clase show-scroll a la etiqueta a con la clase scroll-top
   if (this.scrollY >= 200) scrollTop.classList.add("show-scroll");
   else scrollTop.classList.remove("show-scroll");
 }
@@ -65,19 +65,19 @@ const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
 const iconTheme = "bx-sun";
 
-// Previously selected topic (if user selected)
+// Tema previamente seleccionado (si el usuario lo seleccionó)
 const selectedTheme = localStorage.getItem("selected-theme");
 const selectedIcon = localStorage.getItem("selected-icon");
 
-// We obtain the current theme that the interface has by validating the dark-theme class
+// Obtenemos el tema actual que tiene la interfaz validando la clase dark-theme
 const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
   themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
 
-// We validate if the user previously chose a topic
+// Validamos si el usuario eligió previamente un tema
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  // Si se cumple la validación, preguntamos cuál fue el problema para saber si activamos o desactivamos la oscuridad.
   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
     darkTheme
   );
@@ -86,30 +86,30 @@ if (selectedTheme) {
   );
 }
 
-// Activate / deactivate the theme manually with the button
+// Activar/desactivar el tema manualmente con el botón
 themeButton.addEventListener("click", () => {
-  // Add or remove the dark / icon theme
+  // Agregar o eliminar el tema oscuro/icono
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
+  // Guardamos el tema y el icono actual que eligió el usuario
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
-/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
+/*==================== REDUCE EL TAMAÑO E IMPRIME EN UNA HOJA A4 ====================*/
 
 function scaleCv() {
   document.body.classList.add("scale-cv");
 }
 
-/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
+/*==================== QUITAR EL TAMAÑO CUANDO SE DESCARGUE EL CV====================*/
 
 function removeScale() {
   document.body.classList.remove("scale-cv");
 }
 
 /*==================== GENERATE PDF ====================*/
-// PDF generated area
+// Área generada en PDF
 let areaCv = document.getElementById("area-cv");
 
 let resumeButton = document.getElementById("resume-button");
@@ -117,25 +117,25 @@ let resumeButton = document.getElementById("resume-button");
 // Html2pdf options
 let opt = {
   margin: 0,
-  filename: "myResume.pdf",
+  filename: "pdf_valle.pdf",
   image: { type: "jpeg", quality: 0.98 },
   html2canvas: { scale: 4 },
   jsPDF: { format: "a4", orientation: "portrait" },
 };
 
-// Function to call areaCv and Html2Pdf optionss
+// Función para llamar a las opciones areaCv y Html2Pdf
 function generateResume() {
   html2pdf(areaCv, opt);
 }
 
-// When the button is clicked, it executes the three functions
+// Cuando se hace clic en el botón, ejecuta las tres funciones
 resumeButton.addEventListener("click", () => {
-  // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+  // 1. Se agrega la clase .scale-cv al cuerpo, donde reduce el tamaño de los elementos.
   scaleCv();
 
-  // 2. The PDF is generated
+  // 2. Se genera el PDF
   generateResume();
 
-  // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+  // 3. La clase .scale-cv se elimina del cuerpo después de 5 segundos para volver al tamaño normal.
   setTimeout(removeScale, 5000);
 });
